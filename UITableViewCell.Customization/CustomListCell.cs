e20d38e2df5eede150bc0a482a894e36020f5ba4
moljac <mcvjetko@holisticware.net>
2012-11-14 14:10:28 +0100

    UITableViewCell.Customization sample added

---
+++ UITableViewCell.Customization/CustomListCell.cs
@@ -0,0 +1,51 @@
+
+using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace TEST
+{
+	public partial class CustomListCell : UITableViewCell
+	{  
+		public CustomListCell () : base()
+		{
+		}
+		
+		public CustomListCell (IntPtr handle) : base(handle)
+		{
+		}
+
+		public void UpdateWithData (string text)
+		{  
+			UIColor textColor = UIColor.Black;
+			
+			switch (text.ToLower ()) 
+			{
+			case "green": 
+				textColor = UIColor.Green;
+				break;
+			case "red":
+				textColor = UIColor.Red;
+				break;
+			case "blue": 
+				textColor = UIColor.Blue;
+				break;
+			case "yellow": 
+				textColor = UIColor.Yellow;
+				break;
+			case "purple": 
+				textColor = UIColor.Purple;
+				break;
+			case "orange": 
+				textColor = UIColor.Orange;
+				break;
+			}
+			
+			testButton.SetTitleColor (textColor, UIControlState.Normal);
+		}
+		
+	}
+}
+
