ddaf8c01d46ed7872a6703be12d85515194d9b8a
moljac <mcvjetko@holisticware.net>
2012-11-30 15:17:42 +0100

    new copied XIB

---
+++ UITableViewCellCustomTestVersionInitial/ElementCustom/UITableViewCellCustom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TEST.ElementCustom
+{
+	public class UITableViewCellCustom : MonoTouch.UIKit.UITableViewCell
+	{
+
+		public virtual void UpdateWithData(string name, DateTime time)
+		{
+
+		}
+	}
+}
